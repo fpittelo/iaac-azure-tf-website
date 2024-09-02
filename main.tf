389714b2b1249297508e9b7df2b5dfda5a58bf8f
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2024-09-02 15:04:13 +0200

    AB#149 create websites webapp

--- main.tf
+++ main.tf
@@ -11,3 +11,36 @@ resource "azurerm_service_plan" "wap_sp_website" {
     dept        = var.department
   }
 }
+
+resource "azurerm_linux_web_app" "wap_website" {
+  name                = var.wap_website_name
+  resource_group_name = var.az_rg_name
+  location            = var.az_rg_location
+  service_plan_id     = azurerm_service_plan.wap_sp_website.id
+
+  tags = {
+    project     = var.project
+    owner       = var.owner
+    dept        = var.department
+    status      = var.wap_status_dv
+  }
+
+  site_config {
+    default_documents = ["index.html","index.htm"]
+    application_stack {
+      php_version = "8.2"
+    }
+  }
+}
+
+resource "azurerm_linux_web_app_slot" "dev" {
+  name            = "dev"
+  app_service_id  = azurerm_linux_web_app.wap_website.id
+
+  site_config {
+    default_documents = ["index.html","index.htm"]
+    application_stack {
+      php_version = "8.2"
+    }
+  }
+}
