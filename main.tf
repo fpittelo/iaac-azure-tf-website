22fb1baf0ea9c616de58dbbcdf322be211809705
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-03 08:22:20 +0200

    AB#151 update to main.tf with comments and tags

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+
+######## Creation of Azure Web App  ########
+
+### Creation of Azure Service Plan #########
 resource "azurerm_service_plan" "wap_sp_website" {
   name                = var.wap_sp_name
   location            = var.az_rg_location
@@ -11,7 +15,7 @@ resource "azurerm_service_plan" "wap_sp_website" {
     dept        = var.department
   }
 }
-
+###### Creation of Azure Website ######
 resource "azurerm_linux_web_app" "wap_website" {
   name                = var.wap_website_name
   resource_group_name = var.az_rg_name
@@ -32,11 +36,17 @@ resource "azurerm_linux_web_app" "wap_website" {
     }
   }
 }
-
+##### Creation of Azure Web App Slot qa ####
 resource "azurerm_linux_web_app_slot" "qa" {
   name            = "qa"
   app_service_id  = azurerm_linux_web_app.wap_website.id
-
+  
+  tags = {
+    project     = var.project
+    owner       = var.owner
+    dept        = var.department
+    status      = var.wap_status_dv
+  }
   site_config {
     default_documents = ["index.html","index.htm"]
     application_stack {
@@ -45,9 +55,17 @@ resource "azurerm_linux_web_app_slot" "qa" {
   }
 }
 
+##### Creation of Azure Web App Slot qa ####
 resource "azurerm_linux_web_app_slot" "dev" {
   name            = "dev"
   app_service_id  = azurerm_linux_web_app.wap_website.id
+  
+  tags = {
+    project     = var.project
+    owner       = var.owner
+    dept        = var.department
+    status      = var.wap_status_dv
+  }
 
   site_config {
     default_documents = ["index.html","index.htm"]
