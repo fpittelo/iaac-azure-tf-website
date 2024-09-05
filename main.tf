b69fe15ade14f9543d4bfacfeb8791c197706e91
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-05 10:23:07 +0200

    AB#149 update code

--- main.tf
+++ main.tf
@@ -3,47 +3,47 @@
 
 ######## Creation of Azure Key Vault ########
 
-resource "azurerm_key_vault" "website_vault" {
-  name                              = "alibaba"
-  resource_group_name               = var.az_rg_name
-  location                          = var.az_rg_location
-  tenant_id                         = data.azurerm_client_config.current.tenant_id
-  sku_name                          = "standard"
-  enabled_for_deployment            = true
-  enabled_for_disk_encryption       = false
-  enabled_for_template_deployment   = false
-  soft_delete_retention_days        = 7
-  purge_protection_enabled          = false
-  public_network_access_enabled     = false
+#resource "azurerm_key_vault" "website_vault" {
+# name                              = "alibaba"
+# resource_group_name               = var.az_rg_name
+# location                          = var.az_rg_location
+# tenant_id                         = data.azurerm_client_config.current.tenant_id
+# sku_name                          = "standard"
+# enabled_for_deployment            = true
+# enabled_for_disk_encryption       = false
+# enabled_for_template_deployment   = false
+# soft_delete_retention_days        = 7
+# purge_protection_enabled          = false
+# public_network_access_enabled     = false
 
-  access_policy = [{
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+# access_policy = [{
+#   tenant_id = data.azurerm_client_config.current.tenant_id
+#   object_id = data.azurerm_client_config.current.object_id
 
-    application_id = null  # If not using an application, this can be set to null
+#   application_id = null  # If not using an application, this can be set to null
 
-    certificate_permissions = [
-      "Get",
-      "List",
-      "Create",
-      "Update",
-      "Delete",
-      "Purge",
-    ]
+#   certificate_permissions = [
+#     "Get",
+#     "List",
+#     "Create",
+#     "Update",
+#     "Delete",
+#     "Purge",
+#   ]
 
-    key_permissions = [
-      "Get",  # Add appropriate key permissions here; required even if empty
-    ]
+#   key_permissions = [
+#     "Get",  # Add appropriate key permissions here; required even if empty
+#   ]
 
-    secret_permissions = [
-      "Get",  # Add appropriate secret permissions here; required even if empty
-    ]
+#   secret_permissions = [
+#     "Get",  # Add appropriate secret permissions here; required even if empty
+#   ]
 
-    storage_permissions = [
-      "Get",  # Add appropriate storage permissions here; required even if empty
-    ]
-  }]
-}
+#   storage_permissions = [
+#     "Get",  # Add appropriate storage permissions here; required even if empty
+#   ]
+# }]
+#}
 
 ### Creation of Azure Service Plan #########
 resource "azurerm_service_plan" "wap_sp_website" {
