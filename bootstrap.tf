8b00a67b344597ec817681137db3c1e4b567ec22
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-15 12:46:09 +0200

    massive overhaul of the project with creation of a backend #34 bootstrap correction

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,28 +1,17 @@
 provider "azurerm" {
+  alias   = "bootstrap"
   features {}
-}
-
-variable "az_backend_rg_name" {
-  description = "Resource group name for the Terraform backend storage"
-  type        = string
-}
-
-variable "az_backend_sa_name" {
-  description = "Storage account name for the Terraform backend"
-  type        = string
-}
-
-variable "az_locationlocation" {
-  description = "Location for the resources"
-  type        = string
+  use_oidc = true
 }
 
 resource "azurerm_resource_group" "backend_rg" {
+  provider = azurerm.bootstrap
   name     = var.az_backend_rg_name
-  location = var.az_locationlocation
+  location = var.az_location
 }
 
 resource "azurerm_storage_account" "backend_sa" {
+  provider                  = azurerm.bootstrap
   name                     = var.az_sa_name
   resource_group_name      = var.az_backend_rg_name
   location                 = var.az_location
@@ -31,6 +20,7 @@ resource "azurerm_storage_account" "backend_sa" {
 }
 
 resource "azurerm_storage_container" "backend_container" {
+  provider              = azurerm.bootstrap
   name                  = var.az_backend_container_name
   storage_account_name  = var.az_sa_name
   container_access_type = "private"
