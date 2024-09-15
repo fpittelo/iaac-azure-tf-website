6a907ecabe5dbbd84154206969fbd0eb5bb5312b
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-15 13:48:32 +0200

    #34 update

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -2,20 +2,24 @@
 
 variable "az_backend_rg_name" {
   description = "Resource group name for the Terraform backend storage"
+  default = "backend"
   type        = string
 }
 
 variable "az_backend_sa_name" {
   description = "Storage account name for the Terraform backend"
+  default = "iatbackend"
   type        = string
 }
 
 variable "az_backend_container_name" {
   description = "value of container name"
+  default = "backend"
   type = string
 }
 
 variable "az_location" {
   description = "value of resource group location"
+  default = "SwitzerlandNorth"
   type = string
 }
