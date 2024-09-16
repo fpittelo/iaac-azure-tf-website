eb03ef4a0fdc9fa48ac505d341acaff891b9ccfe
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-16 14:11:39 +0200

    #31 test 5

--- main/providers.tf
+++ main/providers.tf
@@ -19,10 +19,10 @@ terraform {
 provider "azurerm" {
   features {}
   # Use OIDC authentication with the GitHub OIDC service principal
-  use_oidc  = true
-  client_id         = var.az_client_id       # Assuming client_id is passed as a variable if needed
-  tenant_id         = var.az_tenant_id       # Assuming tenant_id is passed as a variable if needed
-  subscription_id   = var.az_subscription_id  # Use the subscription_id variable
+  # use_oidc          = true
+  # client_id         = var.az_client_id       # Assuming client_id is passed as a variable if needed
+  # tenant_id         = var.az_tenant_id       # Assuming tenant_id is passed as a variable if needed
+  # subscription_id   = var.az_subscription_id # Use the subscription_id variable
 }
 
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
