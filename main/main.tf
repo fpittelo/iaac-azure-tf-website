07846dd5476ab49a5bf86a5b6c8640d032509d08
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-17 10:51:04 +0200

    #31 import sa and container

--- main/main.tf
+++ main/main.tf
@@ -16,6 +16,22 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+# Define the existing storage account
+resource "azurerm_storage_account" "backend_sa" {
+  name                     = var.az_backend_container_name   # Ensure this matches the created storage account
+  resource_group_name      = var.az_backend_rg_name
+  location                 = var.az_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Define the existing storage container
+resource "azurerm_storage_container" "backend_container" {
+  name                  = var.az_backend_container_name              # Ensure this matches the created container
+  storage_account_name  = var.az_backend_sa_name
+  container_access_type = "private"
+}
+
 ### Creation of Azure Service Plan #########
 resource "azurerm_service_plan" "wap_sp_website" {
   name                = var.wap_sp_name
