a7b0965eccbe54630d3c6c6c4e6493923b6c398b
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-27 11:54:40 +0200

    #50 correct provider

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,20 +2,27 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "backend4dev"            # Replace with your resource group name
-    storage_account_name  = "backend4dev"         # Replace with your storage account name
-    container_name        = "backend4dev"             # Replace with your container name
-    key                   = "backend4dev.tfstate"     # Replace with your Terraform state file name
   }
 
   required_providers {
     azurerm   = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.8"  # Use the appropriate version
     }
   }
+
+  backend "azurerm" {
+    use_oidc = true
+  } 
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }    
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = false
+    }
+ }
 }
