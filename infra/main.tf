8274a0efd336e1a0f5c4e257713c1f1ee0f20aa1
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-28 13:33:04 +0200

    #50 full modules use

--- infra/main.tf
+++ infra/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 ### Creation of Azure Service Plan #########
-module "azurerm_service_plan" {
+module "app_service_plan" {
   source = "../modules/app_service_plan"
   wap_sp_name           = var.wap_sp_name
   wap_sp_sku            = var.wap_sp_sku
@@ -20,29 +20,32 @@ module "azurerm_service_plan" {
   az_rg_name            = var.az_rg_name
 
   tags = var.tags
+
+  depends_on = [ azurerm_resource_group.rg ]
 }
 
-### Creation of Azure Service Plan #########
-#resource "azurerm_service_plan" "wap_sp_website" {
-# name                = var.wap_sp_name
-# location            = var.az_location
-# resource_group_name = var.az_rg_name
-# sku_name            = var.wap_sp_sku
-#  os_type             = var.wap_sp_sku_os_linux
-#  
-#  depends_on = [azurerm_resource_group.rg]  # Explicit dependency
-#
-#  tags = var.tags
-#}
+###### Creation of Azure Linux Web App ######
+module "linux_web_app" {
+  source = "../modules/linux_web_app"
+  wap_website_name    = var.wap_website_name
+  service_plan_id     = module.app_service_plan.service_plan_id
+  wap_sp_name         = var.wap_sp_name
+  az_rg_name          = var.az_rg_name
+  az_location         = var.az_location
+
+  tags = var.tags
+
+  depends_on = [module.app_service_plan]  # Explicit dependency
+}
 
 ###### Creation of Azure Linux Web App ######
 resource "azurerm_linux_web_app" "wap_website" {
   name                = var.wap_website_name
   resource_group_name = var.az_rg_name
   location            = var.az_location
-  service_plan_id     = module.azurerm_service_plan.azurerm_app_service_plan.service_plan.id
+  service_plan_id     = module.app_service_plan.service_plan.id
 
-  depends_on = [module.azurerm_service_plan]  # Explicit dependency
+  depends_on = [module.app_service_plan]  # Explicit dependency
 
   tags = var.tags
 
@@ -53,42 +56,3 @@ resource "azurerm_linux_web_app" "wap_website" {
     }
   }
 }
-
-##### Creation of Azure Web App Slot qa ####
-#resource "azurerm_linux_web_app_slot" "qa" {
-# name            = "qa"
-# app_service_id  = azurerm_linux_web_app.wap_website.id
-# 
-# tags = {
-#   project     = var.project
-#   owner       = var.owner
-#   dept        = var.department
-#   status      = var.wap_status_dv
-# }
-# site_config {
-#   default_documents = ["index.html","index.htm"]
-#   application_stack {
-#     php_version = "8.2"
-#   }
-# }
-#}
-
-##### Creation of Azure Web App Slot dev ####
-#resource "azurerm_linux_web_app_slot" "dev" {
-# name            = "dev"
-# app_service_id  = azurerm_linux_web_app.wap_website.id
-  
-# tags = {
-#   project     = var.project
-#   owner       = var.owner
-#   dept        = var.department
-#   status      = var.wap_status_dv
-# }
-
-# site_config {
-#   default_documents = ["index.html","index.htm"]
-#   application_stack {
-#     php_version = "8.2"
-#   }
-# }
-#}
